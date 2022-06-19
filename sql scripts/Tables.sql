59366a03e37928f20aebcf40c30bbf836545b801
Saja Ibrahem <74304192+Saja5050@users.noreply.github.com>
2022-06-20 00:00:25 +0300

    last commit

--- sql scripts/Tables.sql
+++ sql scripts/Tables.sql
@@ -1,37 +1,46 @@
-CREATE TABLE Users(
-    user_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(255) NOT NULL,
-    firstname VARCHAR(255),
-    lastname VARCHAR(255),
-    country VARCHAR(255),
-    password VARCHAR(255),
-    email VARCHAR(255),
-    profilePicUrl VARCHAR(255)
-) DEFAULT CHARSET UTF8;
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `firstname` varchar(255) DEFAULT NULL,
+  `lastname` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `profilePicUrl` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3
 
-CREATE TABLE Recipe(  
-    id int NOT NULL AUTO_INCREMENT,
-    title VARCHAR(255),
-    image VARCHAR(255),
-    popularity int,
-    glutenFree BOOLEAN,
-    vegan BOOLEAN,
-    vegetarian BOOLEAN,
-    servings int,
-    recipeOwner VARCHAR(255),
-    ready_in_minutes VARCHAR(255),
-    user_id int,
-    isFamilyRecipe BOOLEAN,
-    instructions text,
-    extended_ingredients text
-    primary key (id, title)
-) DEFAULT CHARSET UTF8;
+CREATE TABLE `recipe` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `popularity` int DEFAULT NULL,
+  `glutenFree` tinyint(1) DEFAULT NULL,
+  `vegan` tinyint(1) DEFAULT NULL,
+  `vegetarian` tinyint(1) DEFAULT NULL,
+  `servings` int DEFAULT NULL,
+  `recipeOwner` varchar(255) DEFAULT NULL,
+  `ready_in_minutes` varchar(255) DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `FamilyRecipe` tinyint(1) DEFAULT NULL,
+  `PersonalRecipe` tinyint(1) DEFAULT NULL,
+  `instructions` text,
+  `extended_ingredients` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
 
-CREATE TABLE `metadata` (
+
+
+CREATE TABLE `favoriterecipes` (
   `user_id` int NOT NULL,
-  `FavoriteRecipes` json DEFAULT NULL,
-  `WatchedRecipes` json DEFAULT NULL,
-  `PersonalRecipes` json DEFAULT NULL,
-  `Meal` json DEFAULT NULL,
-  `FamilyRecipes` json DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+  `recipe_id` int NOT NULL,
+  PRIMARY KEY (`user_id`,`recipe_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
+
+
+CREATE TABLE `watchedrecipes` (
+  `user_id` int NOT NULL,
+  `recipe_id` int NOT NULL,
+  PRIMARY KEY (`user_id`,`recipe_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
+
